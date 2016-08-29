8696414e29def1af3f1e90b74d38baa2b84635d4
Kevin <b03902086@ntu.edu.tw>
2016-08-29 10:01:30 +0800

    fixed bug: sim may be None

--- src/ehownet/two.py
+++ src/ehownet/two.py
@@ -74,7 +74,10 @@ def two(a, b, is_class=True):
     # (2)
     if is_class:
         sim = getSimilarity(a, b) # [-1, 1]
-        conf = 1.0 - ((sim + 1) / 2) # [0, 1]
+        if sim:
+            conf = 1.0 - ((sim + 1) / 2) # [0, 1]
+        else:
+            conf = 0.8
         return 'N', conf
     
     # (3)
