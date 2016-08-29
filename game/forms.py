b6b7c0011b040f9c6ede826a032be75e7151d375
Kevin <b03902086@ntu.edu.tw>
2016-08-29 11:48:03 +0800

    remove global hints (but hints not finished)

--- game/forms.py
+++ game/forms.py
@@ -8,9 +8,11 @@ from crispy_forms.layout import Submit
 class AskForm(forms.Form):
     answer = forms.CharField()
     prev = forms.CharField()
-    question = forms.CharField()
     success = forms.BooleanField(required=False)
     scroll_pos = forms.IntegerField()
+    hints_concat = forms.CharField()
+    used_hints = forms.CharField(required=False)
+    question = forms.CharField()
 
 class AnswerForm(forms.ModelForm):
     class Meta:
